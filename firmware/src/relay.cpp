cf589264c00c03de3b539dfa05bde157ec5638e8
el-crag <azertyggmf@outlook.com>
2024-03-12 19:12:53 -0500

    Serialization for output in JSON inside relay module

--- firmware/src/relay.cpp
+++ firmware/src/relay.cpp
@@ -1,5 +1,7 @@
 #include "Relay.hpp"
 
+#include <ArduinoJson.h>
+
 Relay::Relay(byte pin, int uuid) {
 	Serial.println("new AsyncRelay");
 	taskHandler = nullptr;
@@ -11,28 +13,28 @@ void Relay::run(void* data) {
 	char UUID[6];
 	itoa(this->_delay, UUID, 10);
 	const uint16_t d = this->_delay;
-	Serial.print("Starting: ");
-	Serial.println(UUID);
 
 	TickType_t xDelay = d / portTICK_PERIOD_MS;
 	//~ while(true) {
-		//~ esp_task_wdt_reset();
-		Serial.println("antes del delay");
+		//~ Serial.println("antes del delay");
 		vTaskDelay(xDelay);
-		Serial.println("despues del delay");
+		//~ Serial.println("despues del delay");
 		//~ break;
 		/*if((xTaskGetTickCount() - this->timer) > this->_delay) {
-			Serial.print("{Miguel}: ");
-			Serial.println(UUID);
 			break;
 		}*/
 	//~ }
 
-	Serial.print("Ending: ");
-	Serial.println(UUID);
+	JsonDocument doc;
+
+	doc["cmd"]  = "status";
+	doc["data"]["type"] = "Relay";
+	doc["data"]["uuid"] = this->getUuid();
+	doc["data"]["status"] = "ok";
+
+	serializeJson(doc, Serial);
 
 	//~ this->stop();
 	//Wait a little before killing task
 	vTaskDelete( NULL );
 }
-
